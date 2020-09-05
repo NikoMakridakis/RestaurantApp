cd8ce51a2e56de689fe86adc87b7a5b6badbe5a4
Niko Makridakis <nmak345@gmail.com>
2020-09-05 09:24:52 -0500

    added controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetItems()
+        {
+            return "this will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetItem(int id)
+        {
+            return "this will be one item";
+        }
+    }
+}
